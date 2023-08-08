b53b34c6e5e48c9b30a152f515a8e7cb5717999b
Gertrude Martin <ttbuge@gmail.com>
2023-08-08 18:45:51 +0800

    Create ynbq.js

---
+++ ynbq.js
@@ -0,0 +1,11 @@
+/*
+一念便签：https://apps.apple.com/app/id1473578864
+[rewrite_local]
+^https?:\/\/yinian\.pro\/api\/user url script-response-body https://raw.githubusercontent.com/ttbug/Script/main/ynbq.js
+[MITM]
+hostname = yinian.pro
+*/
+var guding = JSON.parse($response.body);
+guding.data.is_vip = 1;
+guding.data.vip_end_time = "6666-06-06 06:06:06";
+$done({ body: JSON.stringify(guding) });
