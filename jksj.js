77c1fb2c9a5b0a93bea88cb794bc4a9344794fe4
Gertrude Martin <ttbuge@gmail.com>
2023-11-21 17:31:39 +0800

    Update jksj.js

--- jksj.js
+++ jksj.js
@@ -1,12 +1,12 @@
 /*
 
 #小程序://极客时间/GXdRaLCdInCX2Lg
-解锁全课程播放权限
+解锁全课程播放以及文章
 
 [rewrite_local]
 
 
-https://time.geekbang.org/serv/v1/column/articles url script-response-body https://raw.githubusercontent.com/ttbug/Script/main/jksj.js
+https://time.geekbang.org/serv/v1 url script-response-body https://raw.githubusercontent.com/ttbug/Script/main/jksj.js
 
 [mitm] 
 
@@ -17,10 +17,11 @@ hostname = time.geekbang.org
 
 var body = $response.body;
 
-
 body = body.replace(/"article_could_preview":\w+/g, '"article_could_preview":true');
 
 body = body.replace(/article_could_preview":".*?"/g,'article_could_preview":true');
 
+body = body.replace(/is_video_preview":".*?"/g,'is_video_preview":true');
 
+body = body.replace(/column_had_sub":".*?"/g,'column_had_sub":true');
 $done({ body });
