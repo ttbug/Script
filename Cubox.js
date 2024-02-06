a467a3edee64159a6a18cbf6a29c2c5b7968d9af
Gertrude Martin <ttbuge@gmail.com>
2024-02-06 18:23:30 +0800

    Update Cubox.js

--- Cubox.js
+++ Cubox.js
@@ -9,6 +9,5 @@ hostname = cubox.cc, cubox.pro
 */
 var guding = JSON.parse($response.body);
 guding.data.isExpire = false;
-guding.data.expireTime ="6666-06-06T06:06:06Z";
-guding.data.nickName ="https://t.me/Guding88";
+guding.data.expireTime ="2066-06-06T06:06:06Z";
 $done({ body: JSON.stringify(guding) });
