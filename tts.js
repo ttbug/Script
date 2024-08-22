c0f1511752e2ef4b69d1ae009922aa206539b195
Gertrude Martin <ttbuge@gmail.com>
2024-08-22 20:06:07 +0800

    更新 tts.js

--- tts.js
+++ tts.js
@@ -16,9 +16,9 @@ obj = {
   "time" : "2024-08-21 22:42:34.742",
   "status" : 1,
   "data" : {
-    "vip_product_id" : "week",
+    "vip_product_id" : "month",
     "vip_expiry_date" : "2025-08-24 22:36:27",
-    "vip_type" : 0,
+    "vip_type" : 1,
     "invitation_code" : "udu2yd",
     "integral" : 0,
     "vip_expiry_date_1970" : 1724250987000
